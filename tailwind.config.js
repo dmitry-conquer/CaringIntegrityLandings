9364afe2cf17613cfb79682e801333abb8f21190
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-01-09 13:36:07 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: ["./index.html", "./pages/**/*", "./partials/**/*", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {},
+    fontSize: {},
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+};
