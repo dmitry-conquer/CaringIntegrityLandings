2ba79c8143cd2486dd4acddf41ade93024bf83ee
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-09-04 15:00:40 +0300

    Оновлення компоненту Header: додано нову функціональність для обробки підменю, покращено взаємодію з користувачем на сенсорних пристроях, а також внесено зміни у стилі меню для покращення візуального сприйняття.

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -3,13 +3,15 @@ export class Header {
     root: "[data-js-header]",
     overlay: "[data-js-header-overlay]",
     triggerButton: "[data-js-header-trigger-button]",
-    menuLinks: ".menu-item-has-children > a",
+    menuItems: ".menu-item-has-children",
   };
 
   private readonly states: Record<string, string> = {
     isActive: "is-active",
     isLock: "is-lock",
   };
+
+  private itemHasSubmenuElements: HTMLElement[] = [];
 
   private rootElement: HTMLElement | null;
   private overlayElement: HTMLElement | null;
@@ -18,6 +20,9 @@ export class Header {
     this.rootElement = document.querySelector(this.selectors.root);
     this.overlayElement = this.rootElement?.querySelector(this.selectors.overlay) || null;
     this.triggerButtonElement = this.rootElement?.querySelector(this.selectors.triggerButton) || null;
+    this.itemHasSubmenuElements = [
+      ...(this.rootElement?.querySelectorAll(this.selectors.menuItems) || []),
+    ] as HTMLElement[];
   }
 
   public init(): void {
@@ -30,8 +35,47 @@ export class Header {
     document.documentElement.classList.toggle(this.states.isLock);
   };
 
+  private toggleSubmenu(currentIndex: number): void {
+    this.itemHasSubmenuElements.forEach((menuItem, index) => {
+      const subMenu = menuItem.querySelector("ul") as HTMLElement;
+      if (!subMenu) return;
+
+      if (currentIndex === index) {
+        menuItem?.classList.toggle("is-active");
+      } else {
+        menuItem.classList.remove("is-active");
+      }
+    });
+  }
+
+  get isTouchDevice() {
+    return (
+      "ontouchstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      window.matchMedia?.("(pointer: coarse)")?.matches ||
+      window.matchMedia?.("(hover: none)")?.matches
+    );
+  }
+
+  private handleInteraction(index: number, e: MouseEvent) {
+    const menuItem = this.itemHasSubmenuElements[index];
+    const isActive = menuItem.classList.contains("is-active");
+
+    if (!isActive) {
+      e.preventDefault();
+      this.toggleSubmenu(index);
+    }
+  }
+
   private bindEvents(): void {
     this.triggerButtonElement?.addEventListener("click", this.onTriggerButtonClick);
+    this.itemHasSubmenuElements.forEach((item, index) => {
+      item.addEventListener("click", (event: MouseEvent) => {
+        if (this.isTouchDevice) {
+          this.handleInteraction(index, event);
+        }
+      });
+    });
   }
 }
 
